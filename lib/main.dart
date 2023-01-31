113e3956cb7c71bba28959f3dc1bbd897978e667
ParimSsme <parima.ssme@gmail.com>
2023-01-31 12:46:08 +0430

    set up firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flash_chat_starting_project/screens/chat_screen.dart';
 import 'package:flash_chat_starting_project/screens/login_screen.dart';
 import 'package:flash_chat_starting_project/screens/registration_screen.dart';
@@ -5,8 +6,13 @@ import 'package:flash_chat_starting_project/screens/registration_screen.dart';
 import 'constants.dart';
 import 'package:flutter/material.dart';
 import '/screens/welcome_screen.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(FlashChat());
 }
 
