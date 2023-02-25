92c58546e436dd20bd4144df540e71be42080888
ParimSsme <parima.ssme@gmail.com>
2023-02-25 11:15:33 +0430

    refactor codes

---
+++ lib/components/message_bubble.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+
+class MessageBubble extends StatelessWidget {
+  final String? message, sender;
+  const MessageBubble({Key? key, this.message, this.sender}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(bubbleRadius),
+          topRight: Radius.circular(bubbleRadius),
+          bottomLeft: Radius.circular(bubbleRadius),
+          bottomRight: Radius.circular(bubbleRadius),
+        ),
+        color: Colors.blue,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+          child: Column(
+            children: [
+              Text(
+                sender!,
+                style: TextStyle(fontSize: 12, color: kChatEmailColor),
+              ),
+              SizedBox(height: 8),
+              Text(
+                message!,
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
